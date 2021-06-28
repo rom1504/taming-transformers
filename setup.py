6cb58ef5263e3a353c8b34b3101fd3b4516bd554
Romain Beaumont <romain.rom1@gmail.com>
2021-06-28 15:02:54 +0000

    0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='taming-transformers-rom1504',
-    version='0.0.3',
+    version='0.0.4',
     description='Taming Transformers for High-Resolution Image Synthesis',
     packages=find_packages(),
     install_requires=[
